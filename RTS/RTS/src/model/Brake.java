42490dfd7c23376b813b0584069f3a51a7d150fe
Seifeldin-Khalil <96579102+Seifeldin-Khalil@users.noreply.github.com>
2023-04-24 19:57:55 +0200

    Revert "x"
    This reverts commit 660ec82ebd2aea39afc549641b09013a45e3bb3e.

--- RTS/RTS/src/model/Brake.java
+++ RTS/RTS/src/model/Brake.java
@@ -4,28 +4,18 @@
  */
 package model;
 
-import events.Decelerate;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import rts.Config;
 
 /**
  *
  * @author mahmo
  */
-public class Brake extends Thread {
+public class Brake {
     boolean stopCommand;
     double speed;
 
     public Brake() {
     }
-
-    public Brake(double speed) {
-        this.speed = speed;
-    }
-    
-    
 
     public Brake(boolean stopCommand) {
         this.stopCommand = stopCommand;
@@ -40,8 +30,8 @@ public class Brake extends Thread {
     }
     
     public void decelerate (){
-        if (speed > 0) {
-            speed -= random(1, 5);
+        if (speed != 0) {
+            speed -= random(1, 5); 
         } else {
             System.out.println("The chair is already stopped, speed is 0.");;
         }
@@ -58,28 +48,7 @@ public class Brake extends Thread {
         }
         
         Random r = new Random();
-        return r.nextInt((max - min) + 1) + min;
-    }
-    
-    @Override
-    public void run() {
-        while (true) {
-            
-            try {
-                this.sleep(3000);
-                decelerate();
-                if (speed > 0){
-                System.out.println("Your speed is now: " + speed);}
-                else{
-                    System.out.println("Your speed is now: 0"); 
-                }
-                
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Brake.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-            Config.sendEvent(new Decelerate(speed));
-        }
+        return r.nextInt((max - min) + 3) + min;
     }
     
 }
