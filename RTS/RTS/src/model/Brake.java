660ec82ebd2aea39afc549641b09013a45e3bb3e
Seifeldin-Khalil <96579102+Seifeldin-Khalil@users.noreply.github.com>
2023-04-24 18:48:42 +0200

    x

--- RTS/RTS/src/model/Brake.java
+++ RTS/RTS/src/model/Brake.java
@@ -4,18 +4,28 @@
  */
 package model;
 
+import events.Decelerate;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import rts.Config;
 
 /**
  *
  * @author mahmo
  */
-public class Brake {
+public class Brake extends Thread {
     boolean stopCommand;
     double speed;
 
     public Brake() {
     }
+
+    public Brake(double speed) {
+        this.speed = speed;
+    }
+    
+    
 
     public Brake(boolean stopCommand) {
         this.stopCommand = stopCommand;
@@ -30,8 +40,8 @@ public class Brake {
     }
     
     public void decelerate (){
-        if (speed != 0) {
-            speed -= random(1, 5); 
+        if (speed > 0) {
+            speed -= random(1, 5);
         } else {
             System.out.println("The chair is already stopped, speed is 0.");;
         }
@@ -48,7 +58,28 @@ public class Brake {
         }
         
         Random r = new Random();
-        return r.nextInt((max - min) + 3) + min;
+        return r.nextInt((max - min) + 1) + min;
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+            
+            try {
+                this.sleep(3000);
+                decelerate();
+                if (speed > 0){
+                System.out.println("Your speed is now: " + speed);}
+                else{
+                    System.out.println("Your speed is now: 0"); 
+                }
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Brake.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            Config.sendEvent(new Decelerate(speed));
+        }
     }
     
 }
